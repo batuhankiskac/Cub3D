4eb7095f2a17ea3d66766acdbd302c0578b8d051
batuhankiskac <kiskacbatuhan@gmail.com>
2025-08-02 21:25:10 +0300

    Add cub3d.h header file with necessary includes and guards

---
+++ include/cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 21:23:51 by bkiskac           #+#    #+#             */
+/*   Updated: 2025/08/02 21:23:52 by bkiskac          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <errno.h>
+
+#endif
