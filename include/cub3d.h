f8d3f80148b2faa2e4d191c78be621f464360b53
batuhankiskac <kiskacbatuhan@gmail.com>
2025-08-02 21:34:04 +0300

    Update cub3d.h with additional includes and struct definition

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,18 +6,27 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 21:23:51 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/08/02 21:23:52 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/08/02 21:33:49 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "../libft/libft.h"
+# include "../mlx/mlx.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
 # include <unistd.h>
 # include <sys/time.h>
+# include <math.h>
 # include <errno.h>
+
+typedef struct	s_cub3d
+{
+	void	*mlx;
+	void	*win;
+}			t_cub3d;
 
 #endif
