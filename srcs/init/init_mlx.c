a6b6b441cbc8fff186f843e4e50fccf91196c2c9
batuhankiskac <kiskacbatuhan@gmail.com>
2025-08-05 19:15:48 +0300

    Add image initialization in MLX setup

--- srcs/init/init_mlx.c
+++ srcs/init/init_mlx.c
@@ -6,7 +6,7 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 18:17:23 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/08/05 18:21:07 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/08/05 19:13:46 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,12 @@ int	init_mlx(t_cub3d *cub3d)
 		return (init_error());
 	cub3d->win = mlx_new_window(cub3d->mlx, WIN_WIDTH, WIN_HEIGHT, "Cub3D");
 	if (!cub3d->win)
+		return (init_error());
+	cub3d->img.img_ptr = mlx_new_image(cub3d->mlx, WIN_WIDTH, WIN_HEIGHT);
+	if (!cub3d->img.img_ptr)
+		return (init_error());
+	cub3d->img.addr = mlx_get_data_addr(cub3d->img.img_ptr,
+			&cub3d->img.bpp, &cub3d->img.line_len, &cub3d->img.endian);
+	if (!cub3d->img.addr)
 		return (init_error());
 }
