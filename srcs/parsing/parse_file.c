37a0fde410d389fbbb9626a0153d50bfa47149f2
batuhankiskac <kiskacbatuhan@gmail.com>
2025-08-11 11:34:15 +0300

    Update .gitignore to exclude Makefile.gen files; update file headers with correct timestamps and improve map parsing logic

--- srcs/parsing/parse_file.c
+++ srcs/parsing/parse_file.c
@@ -6,7 +6,7 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 14:08:50 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/08/10 18:54:52 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/08/11 11:25:14 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 
 int	parse_file(t_cub3d *cub3d, char *filename)
 {
-	(void)filename;
-	// map structının içeriğini dolduran fonksiyon
-	// devamı yazılacak...
+	t_map	*temp_map;
 
-	// validate map örnek kullanım
+	temp_map = get_filled_t_map(filename);
+	if (!temp_map)
+		return (ERROR);
+	cub3d->map = *temp_map;
+	free(temp_map);
 	if (validate_map(&cub3d->map) == ERROR)
 	{
+		free(cub3d->map.north_texture_path);
+		free(cub3d->map.south_texture_path);
+		free(cub3d->map.west_texture_path);
+		free(cub3d->map.east_texture_path);
 		free_all(cub3d->map.grid);
 		return (ERROR);
 	}
