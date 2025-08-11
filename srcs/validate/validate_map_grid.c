31f1715c4f0797f9a3ec0b4032fd3cafd3e939ca
batuhankiskac <kiskacbatuhan@gmail.com>
2025-08-11 13:35:21 +0300

    Refactor map validation logic: consolidate character checks and player count validation

--- srcs/validate/validate_map_grid.c
+++ srcs/validate/validate_map_grid.c
@@ -6,52 +6,17 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 14:28:06 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/08/06 15:44:56 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/08/11 13:34:15 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static int	is_valid_grid_char(char c)
+static int	check_content(t_map *map)
 {
-	return (c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E'
-		|| c == 'W' || c == ' ');
-}
-
-static int	is_map_closed(t_map *map)
-{
-	int	y;
-	int	x;
-
-	y = -1;
-	while (map->grid[++y])
-	{
-		x = -1;
-		while (map->grid[y][++x])
-		{
-			if (map->grid[y][x] == '0' || ft_strchr("NSEW", map->grid[y][x]))
-			{
-				if (y == 0 || (int)ft_strlen(map->grid[y - 1]) <= x
-					|| map->grid[y - 1][x] == ' ')
-					return (print_error("Map is not closed", ERROR));
-				if (!map->grid[y + 1] || (int)ft_strlen(map->grid[y + 1]) <= x
-					|| map->grid[y + 1][x] == ' ')
-					return (print_error("Map is not closed", ERROR));
-				if (x == 0 || map->grid[y][x - 1] == ' ')
-					return (print_error("Map is not closed", ERROR));
-				if (map->grid[y][x + 1] == '\0' || map->grid[y][x + 1] == ' ')
-					return (print_error("Map is not closed", ERROR));
-			}
-		}
-	}
-	return (0);
-}
-
-static int	check_map_rules(t_map *map)
-{
-	int	y;
-	int	x;
-	int	player_count;
+	int y;
+	int x;
+	int player_count;
 
 	y = -1;
 	player_count = 0;
@@ -60,26 +25,24 @@ static int	check_map_rules(t_map *map)
 		x = -1;
 		while (map->grid[y][++x])
 		{
-			if (!is_valid_grid_char(map->grid[y][x]))
-				return (print_error("Invalid character in map grid", ERROR));
+			if (!ft_strchr(" 01NSEW", map->grid[y][x]))
+				return (print_error("Invalid character in map", ERROR));
 			if (ft_strchr("NSEW", map->grid[y][x]))
 				player_count++;
 		}
 	}
-	if (player_count == 0)
-		return (print_error("No player start position found", ERROR));
-	if (player_count > 1)
-		return (print_error("Multiple player start positions found", ERROR));
+	if (player_count != 1)
+		return (print_error("Map must contain exactly one player", ERROR));
 	return (0);
 }
 
 int	validate_map_grid(t_map *map)
 {
 	if (!map->grid || !map->grid[0])
-		return (print_error("No map grid found", ERROR));
-	if (check_map_rules(map) == ERROR)
+		return (print_error("Map grid not found", ERROR));
+	if (check_content(map) == ERROR)
 		return (ERROR);
-	if (is_map_closed(map) == ERROR)
+	if (execute_fill(map) == ERROR)
 		return (ERROR);
 	return (0);
 }
