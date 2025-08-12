cc5cf8db2ac8644d9225694257c5c49e80b8a00c
batuhankiskac <kiskacbatuhan@gmail.com>
2025-08-12 15:32:53 +0300

    Refactor player movement logic: enhance forward/backward movement calculations and update position validation

--- srcs/engine/player/player_movement.c
+++ srcs/engine/player/player_movement.c
@@ -6,11 +6,25 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 15:09:39 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/08/12 15:16:11 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/08/12 15:32:40 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	move_forward_backward(t_cub3d *cub3d, double move_speed, int dir)
+{
+	double	new_pos_x;
+	double	new_pos_y;
+
+	new_pos_x = cub3d->player.pos_x + dir * cub3d->player.dir_x * move_speed;
+	new_pos_y = cub3d->player.pos_y + dir * cub3d->player.dir_y * move_speed;
+	if (cub3d->map.grid[(int)new_pos_y][(int)cub3d->player.pos_x] != '1')
+		cub3d->player.pos_y = new_pos_y;
+	if (cub3d->map.grid[(int)cub3d->player.pos_y][(int)new_pos_x] != '1')
+		cub3d->player.pos_x = new_pos_x;
+}
+
 
 void	move_player(t_cub3d *cub3d, int keycode)
 {
