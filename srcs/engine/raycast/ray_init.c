25c9e4a04bea880413dba12ff3edd130f5d34301
batuhankiskac <kiskacbatuhan@gmail.com>
2025-08-20 18:51:31 +0300

    Implement ray initialization functions: add calculate_delta_dist and calculate_step_and_side_dist for ray calculations

--- srcs/engine/raycast/ray_init.c
+++ srcs/engine/raycast/ray_init.c
@@ -6,10 +6,59 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 15:00:15 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/08/20 15:39:33 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/08/20 18:38:19 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static void	calculate_delta_dist(t_ray *ray)
+{
+	if (ray->ray_dir_x == 0)
+		ray->delta_dist_x = 1e30;
+	else
+		ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
+	if (ray->ray_dir_y == 0)
+		ray->delta_dist_y = 1e30;
+	else
+		ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
+}
 
+static void	calculate_step_and_side_dist(t_player *player, t_ray *ray)
+{
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (player->pos_x - ray->map_x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - player->pos_x) \
+			* ray->delta_dist_x;
+	}
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (player->pos_y - ray->map_y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - player->pos_y) \
+			* ray->delta_dist_y;
+	}
+}
+
+
+void	ray_init(t_player *player, t_ray *ray, int x)
+{
+	ray->camera_x = 2 * x / (double)WIN_WIDTH - 1;
+	ray->ray_dir_x = player->dir_x + player->plane_x * ray->camera_x;
+	ray->ray_dir_y = player->dir_y + player->plane_y * ray->camera_x;
+	ray->map_x = (int)player->pos_x;
+	ray->map_y = (int)player->pos_y;
+	ray->hit = 0;
+	calculate_delta_dist(ray);
+	calculate_step_and_side_dist(player, ray);
+}
