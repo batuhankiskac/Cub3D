ec8313a42d510ddcf7c2ca96ce784e78f618f41e
Resul Aydoğmuş <39741608+Ustrif@users.noreply.github.com>
2025-08-25 11:53:39 +0000

    map is done inshallah.

--- srcs/parsing/map_check.c
+++ srcs/parsing/map_check.c
@@ -6,28 +6,11 @@
 /*   By: raydogmu <raydogmu@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 06:58:53 by raydogmu          #+#    #+#             */
-/*   Updated: 2025/08/25 07:20:17 by raydogmu         ###   ########.fr       */
+/*   Updated: 2025/08/25 11:45:19 by raydogmu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-static int	check_line_elements(char *s)
-{
-	int	i;
-
-	i = 0;
-	if (!s)
-		return (0);
-	while (s[i])
-	{
-		if (!(s[i] == ' ' || s[i] == '1' || s[i] == '0' || s[i] == 'N'
-			|| s[i] == 'W' || s[i] == 'E' || s[i] == 'S' || s[i] == '\n'))
-			return (0);
-		i++;
-	}
-	return (1);
-}
 
 int	is_first_line_of_map(char **data, int i)
 {
@@ -35,11 +18,12 @@ int	is_first_line_of_map(char **data, int i)
 		return (0);
 	if (ft_strlen(data[i]) <= 1)
 		return (0);
-	while (data[i])
-	{
-		if (!check_line_elements(data[i]))
-			return (0);
-		i++;
-	}
+	if (ft_strnstr(data[i], "NO", ft_strlen(data[i]))
+		|| ft_strnstr(data[i], "EA", ft_strlen(data[i]))
+		|| ft_strnstr(data[i], "WE", ft_strlen(data[i]))
+		|| ft_strnstr(data[i], "SO", ft_strlen(data[i]))
+		|| ft_strnstr(data[i], "F", ft_strlen(data[i]))
+		|| ft_strnstr(data[i], "C", ft_strlen(data[i])))
+		return (0);
 	return (1);
 }
