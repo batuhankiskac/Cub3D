8ee901e0b662d927801c077738d7440d2408d3bd
batuhankiskac <kiskacbatuhan@gmail.com>
2025-10-28 20:48:32 +0300

    refactor: improve player movement calculations for better accuracy

--- srcs/engine/player/player_movement.c
+++ srcs/engine/player/player_movement.c
@@ -6,35 +6,43 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 15:09:39 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/08/21 11:40:42 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/10/28 20:48:00 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	move_forward_backward(t_cub3d *cub3d, double move_speed, int dir)
+static void	move_forward_backward(t_cub3d *cub3d, double speed, int dir)
 {
 	double	new_pos_x;
 	double	new_pos_y;
+	double	c_x;
+	double	c_y;
 
-	new_pos_x = cub3d->player.pos_x + dir * cub3d->player.dir_x * move_speed;
-	new_pos_y = cub3d->player.pos_y + dir * cub3d->player.dir_y * move_speed;
-	if (cub3d->map.grid[(int)new_pos_y][(int)cub3d->player.pos_x] != '1')
+	new_pos_x = cub3d->player.pos_x + dir * cub3d->player.dir_x * speed;
+	new_pos_y = cub3d->player.pos_y + dir * cub3d->player.dir_y * speed;
+	c_x = cub3d->player.pos_x + dir * cub3d->player.dir_x * (speed + 0.1);
+	c_y = cub3d->player.pos_y + dir * cub3d->player.dir_y * (speed + 0.1);
+	if (cub3d->map.grid[(int)c_y][(int)cub3d->player.pos_x] != '1')
 		cub3d->player.pos_y = new_pos_y;
-	if (cub3d->map.grid[(int)cub3d->player.pos_y][(int)new_pos_x] != '1')
+	if (cub3d->map.grid[(int)cub3d->player.pos_y][(int)c_x] != '1')
 		cub3d->player.pos_x = new_pos_x;
 }
 
-static void	move_left_right(t_cub3d *cub3d, double move_speed, int dir)
+static void	move_left_right(t_cub3d *cub3d, double speed, int dir)
 {
 	double	new_pos_x;
 	double	new_pos_y;
+	double	c_x;
+	double	c_y;
 
-	new_pos_x = cub3d->player.pos_x + dir * cub3d->player.plane_x * move_speed;
-	new_pos_y = cub3d->player.pos_y + dir * cub3d->player.plane_y * move_speed;
-	if (cub3d->map.grid[(int)new_pos_y][(int)cub3d->player.pos_x] != '1')
+	new_pos_x = cub3d->player.pos_x + dir * cub3d->player.plane_x * speed;
+	new_pos_y = cub3d->player.pos_y + dir * cub3d->player.plane_y * speed;
+	c_x = cub3d->player.pos_x + dir * cub3d->player.plane_x * (speed + 0.1);
+	c_y = cub3d->player.pos_y + dir * cub3d->player.plane_y * (speed + 0.1);
+	if (cub3d->map.grid[(int)c_y][(int)cub3d->player.pos_x] != '1')
 		cub3d->player.pos_y = new_pos_y;
-	if (cub3d->map.grid[(int)cub3d->player.pos_y][(int)new_pos_x] != '1')
+	if (cub3d->map.grid[(int)cub3d->player.pos_y][(int)c_x] != '1')
 		cub3d->player.pos_x = new_pos_x;
 }
 
